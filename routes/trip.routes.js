dbe8b0ec093b36a26ae750329251bd993db6f824
aasmitha01 <tommandruaasmitha@gmail.com>
2026-01-31 21:11:07 +0530

    Node.js + Express + Supabase

--- routes/trip.routes.js
+++ routes/trip.routes.js
@@ -1,9 +1,8 @@
-import ecpress from "express";
-import { createTrip,endTrip,getTrip,deleteTrip } from "../controllers/trip.controllers.js";
-const r=XPathExpression.Router();
+import express from "express";
+import { createTrip,endTrip,getTrip,deleteTrip } from "../controllers/trip.controller.js";
+const r=express.Router();
 r.post("/create",createTrip);
-r.patch("/ens/:tripId",endTrip);
+r.patch("/end/:tripId",endTrip);
 r.get("/:tripId",getTrip);
 r.delete("/delete/:tripId",deleteTrip);
-
 export default r;
